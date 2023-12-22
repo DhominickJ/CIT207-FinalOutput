7efd0597b9d4a7cd95ff227f2713303d7354e860
Oliver <oliver.ladores@wvsu.edu.ph>
2023-12-22 19:09:30 +0800

    Cleaned up unused stuff and extraneous comments. Didn't do much refactoring.

--- App/App.java
+++ App/App.java
@@ -5,20 +5,18 @@ package App;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.WindowConstants;
-import javax.swing.BorderFactory;
 
 import App.Components.BingoNumber;
 
 import javax.swing.JButton;
 
-import javax.swing.SwingConstants;
 import java.awt.Font;
 import java.awt.Color;
 
 import java.awt.event.*;
+
 /**
  * App
  */
@@ -27,6 +25,7 @@ public class App {
   public static void main(String[] args) {
     javax.swing.SwingUtilities.invokeLater(App::buildGUI);
   }
+
   static void buildGUI() {
     var appWindow = new JFrame("BINGO Game!");
     var bingoBoard = new JPanel(new GridLayout(1, 5));
@@ -59,12 +58,22 @@ public class App {
     String letter_color = "black";
     String colored_circle;
     int counter = 0;
-    switch (letter){
-      case "B": letter_color = "cyan"; break;
-      case "I": letter_color = "blue"; break;
-      case "N": letter_color = "red"; break;
-      case "G": letter_color = "yellow"; break;
-      case "O": letter_color = "light_green"; break;
+    switch (letter) {
+      case "B":
+        letter_color = "cyan";
+        break;
+      case "I":
+        letter_color = "blue";
+        break;
+      case "N":
+        letter_color = "red";
+        break;
+      case "G":
+        letter_color = "yellow";
+        break;
+      case "O":
+        letter_color = "light_green";
+        break;
     }
     colored_circle = "light_" + letter_color;
     var bingoLabel = components.new BingoNumber(letter, "");
@@ -74,41 +83,23 @@ public class App {
 
     for (int bingo_Num : rand_Num) {
       BingoNumber bingoButton;
-      if(letter == "N" && counter == 2){
+      if (letter == "N" && counter == 2) {
         bingoButton = components.new BingoNumber("FREE", "gold");
-      }
-      else{
+      } else {
         bingoButton = components.new BingoNumber(Integer.toString(bingo_Num), colored_circle);
       }
-      
-      // int b_count = 0;
-      // int i_count = 0;
-      // int n_count = 0;
-      // int g_count = 0;
-      // int o_count = 0;
 
       bingoButton.addActionListener(new ActionListener() {
         @Override
         public void actionPerformed(ActionEvent e) {
           // Handle the button click here
-          System.out.println("Button clicked!");
           BingoNumber btn;
           btn = components.new BingoNumber(Integer.toString(bingo_Num), colored_circle);
           btn.borderStatus(false);
-          JButton button = (JButton)e.getSource();
+          JButton button = (JButton) e.getSource();
           // // Change text color to black and border color to gray when selected
           button.setForeground(Color.lightGray);
           button.setBackground(Color.GRAY);
-          // switch(letter){
-          //   case "B" : b_count =+ 1; break;
-          //   case "I" : i_count =+ 1; break;
-          //   case "N" : n_count =+ 1; break;
-          //   case "G" : g_count =+ 1; break;
-          //   case "O" : g_count =+ 1; break;
-          // }
-          
-          // // ((CustomButton)btn).borderStatus(false);
-          // btn.borderStatus(false);
 
         }
       });
@@ -117,43 +108,6 @@ public class App {
       counter += 1;
     }
 
-
     return bingoLane;
   }
 }
-//   public boolean checkBingo(boolean[][] card) {
-//         int size = card.length;
-
-//         // Check rows and columns
-//         for (int i = 0; i < size; i++) {
-//           boolean rowBingo = true;
-//           boolean colBingo = true;
-//           for (int j = 0; j < size; j++) {
-//             if (!card[i][j]) {
-//               rowBingo = false;
-//             }
-//             if (!card[j][i]) {
-//               colBingo = false;
-//             }
-//           }
-//           if (rowBingo || colBingo) {
-//             return true;
-//           }
-//         }
-
-//         // Check diagonals
-//         boolean diagBingo1 = true;
-//         boolean diagBingo2 = true;
-//         for (int i = 0; i < size; i++) {
-//           if (!card[i][i]) {
-//             diagBingo1 = false;
-//           }
-//           if (!card[i][size - i - 1]) {
-//             diagBingo2 = false;
-//           }
-//         }
-
-//         return diagBingo1 || diagBingo2;
-//       }
-// }
-
--- App/Components.java
+++ App/Components.java
@@ -1,6 +1,5 @@
 package App;
 
-// import javax.lang.model.type.NullType;
 import javax.swing.*;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -8,12 +7,8 @@ import java.awt.Dimension;
 import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Font;
-import java.awt.event.*;
-import java.awt.geom.Ellipse2D;
 import java.awt.geom.RoundRectangle2D;
-import java.awt.Shape;
 import java.awt.*;
-// import java.shape.Ellipse2D;
 
 public class Components {
   public class BingoNumber extends JButton {
@@ -21,12 +16,13 @@ public class Components {
     private String color;
     private boolean statusBorder = true;
 
+    // Override ctor to add additional behavior.
     public BingoNumber(String text, String color) {
       super(text);
       int counter = 0;
       this.setFont(new Font("Impact", Font.PLAIN, text.equals("FREE") ? 20 : 50));
       this.setForeground(stringToColor(color));
-      if(counter == 0){
+      if (counter == 0) {
         this.setBackground(getBackground());
       } else {
         this.setBorder(BorderFactory.createLineBorder(stringToColor(color), 5));
@@ -42,181 +38,137 @@ public class Components {
     public void changeFont(String fontName, int style, int size) {
       this.setFont(new Font(fontName, style, size));
     }
-    public void changeFontSize(int size){
-      this.changeFont("Impact",Font.BOLD,size);
+
+    public void changeFontSize(int size) {
+      this.changeFont("Impact", Font.BOLD, size);
     }
 
-    public void changeTextColor(String color){
+    public void changeTextColor(String color) {
       this.setForeground(stringToColor(color));
     }
 
-    public void coloredBorder(int size){
+    public void coloredBorder(int size) {
       this.setBorder(BorderFactory.createLineBorder(stringToColor(color), size));
     }
-    public void borderStatus(boolean statusBorder){
+
+    public void borderStatus(boolean statusBorder) {
       this.statusBorder = statusBorder;
       // this.repaint();
     }
 
-
+    // Overriding the paintComponent function to fit our design goals.
     @Override
     protected void paintComponent(Graphics g) {
-    float thickness = 3f;
-    // If statusBorder is false, remove the border
-    if (!statusBorder) {
+      float thickness = 3f;
+      // If statusBorder is false, remove the border
+      if (!statusBorder) {
         setBorder(null);
-    } else {
+      } else {
         // If statusBorder is true, set the border to your custom border
         setBorder(BorderFactory.createStrokeBorder(new BasicStroke(thickness)));
-    }
+      }
       super.paintComponent(g);
-      int diameter = Math.min(getSize().width, getSize().height) - 20;
       int rect_size = Math.min(getSize().width, getSize().height) - 20;
       int x_rect = (getSize().width - rect_size) / 2;
       int y_rect = (getSize().height - rect_size) / 2;
-      // int x = (getSize().width - diameter) / 2;
-      // int y = (getSize().height - diameter) / 2;
 
       Graphics2D g2d = (Graphics2D) g;
-
-      // g2d.drawRect(x, y, diameter, diameter);
 
       // Enable anti-aliasing
       g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
       // Draw a rectangle with rounded corners
       float roundness = 35f; // Adjust this value to change the roundness of the corners
-      g2d.setColor(stringToColor(color));      
-      // float thickness = 3f; // Set the thickness of the border here
+      g2d.setColor(stringToColor(color));
       g2d.setStroke(new BasicStroke(thickness));
 
       if (getModel().isArmed()) {
         g2d.setColor(Color.lightGray);
       } else {
-        // g.setColor(getBackground());
         g2d.setColor(getBackground());
       }
-      // g2d.setStroke(new BasicStroke(thickness));
       g2d.fill(new RoundRectangle2D.Float(x_rect, y_rect, rect_size, rect_size, roundness, roundness));
-      // g.fillRect(x, y, diameter, diameter);
-      // g2d.draw(new RoundRectangle2D.Float(x_rect, y_rect, rect_size, rect_size, roundness, roundness));
     }
-    
 
     @Override
     protected void paintBorder(Graphics g) {
       super.paintComponent(g);
-        int diameter = Math.min(getSize().width, getSize().height) - 20;
-        int rect_size = Math.min(getSize().width, getSize().height) - 10;
-        // int x_rect = (getSize().width - rect_size) / 2;
-        // int y_rect = (getSize().height - rect_size) / 2;
-        int x = (getSize().width - diameter) / 2;
-        int y = (getSize().height - diameter) / 2;
+      int diameter = Math.min(getSize().width, getSize().height) - 20;
+      int x = (getSize().width - diameter) / 2;
+      int y = (getSize().height - diameter) / 2;
 
-        Graphics2D g2d = (Graphics2D) g;
-        g2d.setColor(stringToColor(color));
-        float thickness = 3f; // Set the thickness of the border here
-        g2d.setStroke(new BasicStroke(thickness));
-        // g2d.drawRect(x, y, diameter, diameter);
+      Graphics2D g2d = (Graphics2D) g;
+      g2d.setColor(stringToColor(color));
+      float thickness = 3f; // Set the thickness of the border here
+      g2d.setStroke(new BasicStroke(thickness));
+      // g2d.drawRect(x, y, diameter, diameter);
 
-        // Enable anti-aliasing
-        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+      // Enable anti-aliasing
+      g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
-        // Draw a rectangle with rounded corners
-        float roundness = 35f; // Adjust this value to change the roundness of the corners'
+      // Draw a rectangle with rounded corners
+      float roundness = 35f; // Adjust this value to change the roundness of the corners'
 
       if (getModel().isArmed()) {
         g2d.setColor(Color.lightGray);
-      }
-      else{
+      } else {
         g2d.setColor(stringToColor(color));
       }
 
-        g2d.draw(new RoundRectangle2D.Float(x, y, diameter, diameter, roundness, roundness));
-        // g2d.setStroke(new BasicStroke(thickness));
-        // g2d.fill(new RoundRectangle2D.Float(x_rect, y_rect, rect_size, rect_size, roundness, roundness));
-      }
+      g2d.draw(new RoundRectangle2D.Float(x, y, diameter, diameter, roundness, roundness));
+      // g2d.setStroke(new BasicStroke(thickness));
+      // g2d.fill(new RoundRectangle2D.Float(x_rect, y_rect, rect_size, rect_size,
+      // roundness, roundness));
+    }
 
-
-    // Shape shape;
-    // public boolean contains(int x, int y) {
-    //   if (shape == null || 
-    //     !shape.getBounds().equals(getBounds())) {
-    //     shape = new Ellipse2D.Float(0, 0, getWidth(), getHeight());
-    //   }
-    //   return shape.contains(x, y);
-    // }
-
-      // g.setColor(stringToColor(color));
-      // g.drawOval(x, y, diameter, diameter);
-
-    // @Override
-    // public boolean contains(int x, int y) {
-    //   // Check if the point is within the circle
-    //   int radius = getSize().width / 2;
-    //   return Point2D.distance(x, y, radius, radius) < radius;
-    // }
-    // @Override
-    // protected void paintComponent(Graphics g) {
-    //   // super.paintComponent(g);
-    //   // int diameter = Math.min(getSize().width, getSize().height) - 20;
-    //   // int rect_size = Math.min(getSize().width, getSize().height);
-    //   // int x_rect = (getSize().width - rect_size) / 2;
-    //   // int y_rect = (getSize().height - rect_size) / 2;
-    //   // int x = (getSize().width - diameter) / 2;
-    //   // int y = (getSize().height - diameter) / 2;
-
-    //   // Graphics2D g2 = (Graphics2D) g;
-    //   // g2.setStroke(new BasicStroke(4));
-
-    //   // // Draw rectangle
-    //   // g.setColor(getBackground());
-    //   // g.fillRect(x, y, rect_size, rect_size);
-
-    //   // Draw border
-    //   // g.setColor(stringToColor(color));
-    //   // g.drawRect(x_rect, y_rect, rect_size, rect_size);
-
-    // if (getModel().isArmed()) {
-    //     g.setColor(Color.LIGHT_GRAY);
-    // } else {
-    //     g.setColor(getBackground());
-    // }
-    // g.fillOval(0, 0, getSize().width-1, getSize().height-1);
-    // super.paintComponent(g);
-    // }
-      
-      // g.setColor(stringToColor(color));
-      // g.fillOval(x, y, diameter, diameter);
-      // super.paintComponent(g);
-
-    public void paintLabel(Graphics g, String color){
+    public void paintLabel(Graphics g, String color) {
       g.setColor(stringToColor(color));
     }
 
     private Color stringToColor(String color) {
       switch (color.toLowerCase()) {
-        case "black": return Color.BLACK;
-        case "blue": return new Color(52, 204, 255);
-        case "light_blue": return new Color(52, 204, 255);
-        case "cyan": return Color.CYAN;
-        case "light_cyan": return new Color(110, 225, 255);
-        case "dark_gray": return Color.DARK_GRAY;
-        case "gray": return Color.GRAY;
-        case "green": return Color.GREEN;
-        case "light_gray": return Color.LIGHT_GRAY;
-        case "magenta": return Color.MAGENTA;
-        case "orange": return Color.ORANGE;
-        case "pink": return Color.PINK;
-        case "red": return Color.RED;
-        case "light_red": return new Color(204, 77, 77);
-        case "white": return Color.WHITE;
-        case "yellow": return Color.YELLOW;
-        case "light_yellow": return new Color(255, 255, 113);
-        case "light_green": return new Color(0, 250, 154);
-        case "light_light_green": return new Color(0, 255, 153);
-        case "gold": return new Color(229, 184, 11);
-        default: return null;
+        case "black":
+          return Color.BLACK;
+        case "blue":
+          return new Color(52, 204, 255);
+        case "light_blue":
+          return new Color(52, 204, 255);
+        case "cyan":
+          return Color.CYAN;
+        case "light_cyan":
+          return new Color(110, 225, 255);
+        case "dark_gray":
+          return Color.DARK_GRAY;
+        case "gray":
+          return Color.GRAY;
+        case "green":
+          return Color.GREEN;
+        case "light_gray":
+          return Color.LIGHT_GRAY;
+        case "magenta":
+          return Color.MAGENTA;
+        case "orange":
+          return Color.ORANGE;
+        case "pink":
+          return Color.PINK;
+        case "red":
+          return Color.RED;
+        case "light_red":
+          return new Color(204, 77, 77);
+        case "white":
+          return Color.WHITE;
+        case "yellow":
+          return Color.YELLOW;
+        case "light_yellow":
+          return new Color(255, 255, 113);
+        case "light_green":
+          return new Color(0, 250, 154);
+        case "light_light_green":
+          return new Color(0, 255, 153);
+        case "gold":
+          return new Color(229, 184, 11);
+        default:
+          return null;
       }
     }
 
@@ -224,17 +176,5 @@ public class Components {
     public Dimension getPreferredSize() {
       return new Dimension(50, 50);
     }
-    private void triggerAction() {
-        // Create the action event
-        ActionEvent event = new ActionEvent(this, ActionEvent.ACTION_PERFORMED, "button clicked");
-
-        // Get the action listeners
-        ActionListener[] listeners = this.getActionListeners();
-
-        // Call actionPerformed on each listener
-        for (ActionListener listener : listeners) {
-            listener.actionPerformed(event);
-        }
-    }
   }
-} 
+}
